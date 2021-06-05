7691ce0ed2e4499a6ef5cbe5c3f567287931173f
okubomasaya <ooookubo.48@gmail.com>
2021-06-05 09:46:27 +0000

    [finish]4

--- favorite_comment/app/models/user.rb
+++ favorite_comment/app/models/user.rb
@@ -6,7 +6,27 @@ class User < ApplicationRecord
 
   has_many :books
 	has_many :favorites, dependent: :destroy
-	has_many :book_comments, dependent: :destroy
+
+	has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+	has_many :followers, through: :reverse_of_relationships, source: :follower
+	#followed_idをフォローしている人
+
+	has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :followings, through: :relationships, source: :followed
+  #follower_idをフォローしている人
+
+  def follow(user_id)
+    unless self == user_id
+      relationships.create(followed_id: user_id)
+    end
+  end
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    followings.include?(user)
+  end
+
   attachment :profile_image, destroy: false
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
